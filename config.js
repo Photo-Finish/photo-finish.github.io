48b47e5b3369f6da695b6d95b2a37df325009e39
Amphoreus Guard <guard@amphoreus.local>
2026-08-16 21:32:11 +0800

    front door -> https://combines-features-alexander-chains.trycloudflare.com

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AMPHOREUS = {"status": "https://oregon-screensavers-trunk-lobby.trycloudflare.com", "ui": "https://vocal-correlation-paths-certification.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:29:38"};
+window.AMPHOREUS = {"status": "https://combines-features-alexander-chains.trycloudflare.com", "ui": "https://explore-harmony-kevin-sixth.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:32:11"};
