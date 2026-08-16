91e19ba443e29bbc711d5edcb0f671e9ebd2f6f7
Amphoreus Guard <guard@amphoreus.local>
2026-08-16 21:11:04 +0800

    front door -> https://temperatures-dave-picture-glen.trycloudflare.com

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AMPHOREUS = {"status": "https://dallas-jewelry-gathering-notified.trycloudflare.com", "ui": "https://flows-exchange-univ-themselves.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:09:18"};
+window.AMPHOREUS = {"status": "https://temperatures-dave-picture-glen.trycloudflare.com", "ui": "https://metallic-easy-forests-authority.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:11:04"};
