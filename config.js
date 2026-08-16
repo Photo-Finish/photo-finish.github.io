066ce1cc260268309aacf6a9b23a057d9fb642e7
Amphoreus Guard <guard@amphoreus.local>
2026-08-16 21:05:08 +0800

    front door -> https://knit-jean-dense-topic.trycloudflare.com

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AMPHOREUS = {"status": "", "ui": "", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:04:52"};
+window.AMPHOREUS = {"status": "https://knit-jean-dense-topic.trycloudflare.com", "ui": "https://thy-partly-float-flux.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:05:08"};
