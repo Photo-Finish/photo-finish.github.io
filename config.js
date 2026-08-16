5ee159f81860ebb628ada042088caa946acee1d8
Amphoreus Guard <guard@amphoreus.local>
2026-08-16 21:06:35 +0800

    front door -> https://additions-constitutional-radical-reno.trycloudflare.com

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AMPHOREUS = {"status": "https://knit-jean-dense-topic.trycloudflare.com", "ui": "https://thy-partly-float-flux.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:05:08"};
+window.AMPHOREUS = {"status": "https://additions-constitutional-radical-reno.trycloudflare.com", "ui": "https://ebony-respondents-excessive-sand.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:06:35"};
