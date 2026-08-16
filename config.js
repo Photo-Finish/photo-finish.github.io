15622218b825f11b5d56f4ba4fc1d658610cbf79
Amphoreus Guard <guard@amphoreus.local>
2026-08-16 21:29:38 +0800

    front door -> https://oregon-screensavers-trunk-lobby.trycloudflare.com

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AMPHOREUS = {"status": "https://movies-generations-taxi-warren.trycloudflare.com", "ui": "https://score-mia-draw-after.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:25:16"};
+window.AMPHOREUS = {"status": "https://oregon-screensavers-trunk-lobby.trycloudflare.com", "ui": "https://vocal-correlation-paths-certification.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-16T21:29:38"};
