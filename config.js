510f5a547f4eaecb6094a52b08b94f5188a8d7c6
Amphoreus Guard <guard@amphoreus.local>
2026-08-18 20:10:11 +0800

    front door -> https://walking-delayed-cuts-celebs.trycloudflare.com

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AMPHOREUS = {"status": "https://walking-delayed-cuts-celebs.trycloudflare.com", "ui": "https://walt-data-hydrogen-marco.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-18T20:08:50"};
+window.AMPHOREUS = {"status": "https://walking-delayed-cuts-celebs.trycloudflare.com", "ui": "https://walt-data-hydrogen-marco.trycloudflare.com", "lan_status": "http://Lambda.local:8765", "lan_ui": "http://Lambda.local:8501", "updated": "2026-08-18T20:10:11"};
